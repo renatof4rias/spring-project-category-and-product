f5b8c7465ae52a1fe9369bf9e8a77397c5c44c17
renatof4rias <renatof4rias@gmail.com>
2024-01-03 19:19:46 -0300

    Projeto Iniciado e finalizado

---
+++ src/main/java/com/softelse/config/TestConfig.java
@@ -0,0 +1,52 @@
+package com.softelse.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.softelse.entities.Category;
+import com.softelse.entities.Product;
+import com.softelse.repositories.CategoryRepository;
+import com.softelse.repositories.ProductRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner{
+
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+
+		Category cat1 = new Category(null, "Eletronics");
+		Category cat2 = new Category(null, "Books");
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		Product p1 = new Product(null, "TV", 2200.00);
+		Product p2 = new Product(null, "Domain Driven Desing", 120.00);
+		Product p3 = new Product(null, "PS5", 2800.00);
+		Product p4 = new Product(null, "Docker", 100.00);
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4));
+		
+		p1.setCategory(cat1);
+		p2.setCategory(cat2);
+		p3.setCategory(cat1);
+		p4.setCategory(cat2);
+		
+//		cat1.getProducts().add(p1);
+//		cat1.getProducts().add(p3);
+//		cat2.getProducts().add(p2);
+//		cat2.getProducts().add(p4);
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4));
+		
+	}
+}
---
+++ src/main/java/com/softelse/entities/Product.java
@@ -0,0 +1,89 @@
+package com.softelse.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_product")
+public class Product implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Double price;
+	
+	@ManyToOne
+	@JoinColumn(name = "category_id")
+	private Category category;
+	
+	public Product() {
+		
+	}
+
+	public Product(Long id, String name, Double price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
